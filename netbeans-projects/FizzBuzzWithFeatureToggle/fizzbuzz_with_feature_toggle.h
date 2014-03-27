a01dc55b30c6fc98b835e6c3debe1d9eb7bdc2a4
Ben Wu <wubin28@gmail.com>
2014-03-27 18:50:34 +0800

    Added interface string handle(int number) of class FizzBuzzHandler in the header file.

--- netbeans-projects/FizzBuzzWithFeatureToggle/fizzbuzz_with_feature_toggle.h
+++ netbeans-projects/FizzBuzzWithFeatureToggle/fizzbuzz_with_feature_toggle.h
@@ -9,9 +9,13 @@
 #define	FIZZBUZZ_WITH_FEATURE_TOGGLE_H
 
 #include "gtest/gtest.h"
+#include <string>
+
+using std::string;
 
 class FizzBuzzHandler {
-    
+public:
+    string handle(int number);
 };
 #endif	/* FIZZBUZZ_WITH_FEATURE_TOGGLE_H */
 
