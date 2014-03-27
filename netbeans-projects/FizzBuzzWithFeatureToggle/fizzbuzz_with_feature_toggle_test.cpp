1e4a8606e05afcd6fa73646a1e1a54887f2ef0f6
Ben Wu <wubin28@gmail.com>
2014-03-27 21:04:10 +0800

    Extracted the instantiation of FizzBuzzHandler to the constructor of the test class.

--- netbeans-projects/FizzBuzzWithFeatureToggle/fizzbuzz_with_feature_toggle_test.cpp
+++ netbeans-projects/FizzBuzzWithFeatureToggle/fizzbuzz_with_feature_toggle_test.cpp
@@ -5,41 +5,43 @@ namespace {
 // The fixture for testing class FizzBuzzWithFeatureToggle.
 class FizzBuzzWithFeatureToggleTest : public ::testing::Test {
 protected:
-  // You can remove any or all of the following functions if its body
-  // is empty.
+    // You can remove any or all of the following functions if its body
+    // is empty.
+
+  FizzBuzzHandler *ptrFizzBuzzHandler = NULL;
 
   FizzBuzzWithFeatureToggleTest() {
-    // You can do set-up work for each test here.
+      // You can do set-up work for each test here.
+      FizzBuzzHandler fizzBuzzHandler;
+      ptrFizzBuzzHandler = &fizzBuzzHandler;
   }
 
   virtual ~FizzBuzzWithFeatureToggleTest() {
-    // You can do clean-up work that doesn't throw exceptions here.
+      // You can do clean-up work that doesn't throw exceptions here.
   }
 
   // If the constructor and destructor are not enough for setting up
   // and cleaning up each test, you can define the following methods:
 
   virtual void SetUp() {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
+      // Code here will be called immediately after the constructor (right
+      // before each test).
   }
 
   virtual void TearDown() {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
+      // Code here will be called immediately after each test (right
+      // before the destructor).
   }
 
   // Objects declared here can be used by all tests in the test case for FizzBuzzWithFeatureToggle.
 };
 
 TEST_F(FizzBuzzWithFeatureToggleTest, When1Then1) {
-  FizzBuzzHandler fizzBuzzHandler;
-  EXPECT_EQ("1", fizzBuzzHandler.handle(1));
+    EXPECT_EQ("1", ptrFizzBuzzHandler->handle(1));
 }
 
 TEST_F(FizzBuzzWithFeatureToggleTest, When3ThenFizz) {
-    FizzBuzzHandler fizzBuzzHandler;
-    EXPECT_EQ("Fizz", fizzBuzzHandler.handle(3));
+    EXPECT_EQ("Fizz", ptrFizzBuzzHandler->handle(3));
 }
 
 }  // namespace
